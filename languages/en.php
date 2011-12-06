88290becc0d96a6ade112b922ceb55b017f47313
Jerome Bakker <jeabakker@coldtrick.com>
2011-12-06 12:10:28 +0100

    fixed: some language inconsistencies

--- languages/en.php
+++ languages/en.php
@@ -2,13 +2,13 @@
 
 	$english = array(
 	
-		'croncheck:info' => 'Latest Cron runs',
-		'croncheck:registered' => 'Events registered per cron interval',
+		'croncheck:info' => 'Latest CRON runs',
+		'croncheck:registered' => 'Events registered per CRON interval',
 		'croncheck:interval' => 'Interval',
 		'croncheck:timestamp' => 'Timestamp',
 		'croncheck:friendly_time' => 'Friendly time',	
 	
-		'croncheck:no_run' => 'This cron has never run before (since this plugin is enabled)',
+		'croncheck:no_run' => 'This CRON has never run before (since this plugin was enabled)',
 		'croncheck:none_registered' => 'No events are registered for this interval',
 		
 		// widget
@@ -16,4 +16,4 @@
 		'croncheck:widget:description' => "Shows information about CRON, when did it run last and which functions are registered to the different intervals",
 	);
 					
-	add_translation("en",$english);
+	add_translation("en", $english);
