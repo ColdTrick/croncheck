93aa04189799547572e97cbd93d0236e977e5b77
dragonjon <jonaltuna@msn.com>
2017-10-07 18:22:38 +0200

    Spanish translation

---
+++ languages/es.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file was created by Translation Editor v6.0
+ * On 2017-10-07 18:18
+ */
+
+return array (
+  'croncheck:info' => 'Última ejecución de Cron',
+  'croncheck:registered' => 'Eventos registrados por intervalo de cron',
+  'croncheck:events' => 'Eventos',
+  'croncheck:interval' => 'Intervalo',
+  'croncheck:timestamp' => 'Marca de tiempo',
+  'croncheck:friendly_time' => 'Tiempo amistoso',
+  'croncheck:no_run' => 'Este cron nunca ha funcionado antes.',
+  'croncheck:none_registered' => 'Este cron nunca ha funcionado antes.',
+  'admin:statistics:cron' => 'Cron',
+);
